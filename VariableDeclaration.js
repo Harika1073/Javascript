0968702d3aa4ab11223ed71126f07b5d82c5d48e
Harika1073 <pandurenuharisailu143@gmail.com>
2024-12-23 16:37:32 +0530

    updated code

--- VariableDeclaration.js
+++ VariableDeclaration.js
@@ -1,1 +1,9 @@
-console.log("Hello Welcome to Javascript")
+//var , let and const
+
+
+
+{
+    var a = 10;
+    console.log('Accesing a inside the block',a );
+}
+console.log('Access a outside the block', a);
