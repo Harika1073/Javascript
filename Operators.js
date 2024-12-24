b95376a7db96eeef1b60b13a5eea248230fa4a79
Harika1073 <pandurenuharisailu143@gmail.com>
2024-12-24 11:39:20 +0530

    added operators file

---
+++ Operators.js
@@ -0,0 +1,15 @@
+// console.log(10+20);
+// console.log(10-20);
+// console.log(2 ** 2); //returns power
+// var a = 30;
+// console.log(a++);
+// console.log(a);
+// console.log(++a);
+
+
+//== operator checks only value not data type
+console.log(10 == 10); // true
+console.log(10 == '10'); //true
+
+//=== checks value and data types
+console.log(10 === '10');
