0702b5fbe252c06f49c9f2c29eaf13458076090e
Harika1073 <pandurenuharisailu143@gmail.com>
2024-12-24 09:49:56 +0530

    added different ways of function declaration

--- VariableDeclaration.js
+++ VariableDeclaration.js
@@ -1,9 +1,10 @@
 //var , let and const
 
-
-
 {
     var a = 10;
     console.log('Accesing a inside the block',a );
 }
 console.log('Access a outside the block', a);
+
+let score = 20;
+console.log("Updated Score:", score);
---
+++ diff-ways-of-function-declaration.js
@@ -0,0 +1,21 @@
+function add(a,b) {
+console.log(a + b);
+}
+add(100,200);
+
+// Arrow Expression
+
+const sub =function(a,b) {
+    console.log(a-b);
+}
+sub(300,200);
+
+// arrow function
+
+const mul = (a,b) => console.log(a * b)
+mul(10,20)
+
+const msg = ()=> "Hello world";
+let res = msg();
+console.log(res);
+
