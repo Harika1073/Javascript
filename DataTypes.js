04a88586a846d84db467ba4f20b08d7e57635097
Harika1073 <pandurenuharisailu143@gmail.com>
2024-12-24 10:12:20 +0530

    dataypes

---
+++ DataTypes.js
@@ -0,0 +1,26 @@
+//number DataType:
+var a = 90
+console.log(a, typeof a);
+//number Datatype: All numbers(whole, decimal,positive, negative)
+
+var b = 20.332451235;
+console.log(b ,typeof b);
+
+
+var c = 'h';
+console.log(c, typeof c);
+//Strings: single character or series of characters of all are strings
+
+var d = true;
+console.log(d, typeof d); //boolean
+
+var e = "harika";
+console.log(e , typeof e);
+
+let f;
+console.log(f);//undefined
+
+let g = null;
+console.log(g);//null
+
+
